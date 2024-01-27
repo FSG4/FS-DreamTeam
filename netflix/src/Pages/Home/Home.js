3cb954fe6bf8756801f87d530384ab1e792b3e15
eliyas41 <fentaeliyas@gmail.com>
2024-01-27 09:56:09 +0300

    add row section

--- netflix/src/Pages/Home/Home.js
+++ netflix/src/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../Componets/Header/Header'
 import Footer from '../../Componets/Footer/Footer'
+import RowList from "../../Componets/Rows/RowList/RowList"
 import Banner from '../../Componets/Banner/Banner'
 function Home() {
   return (
     <>
     <Header/> 
     <Banner />
+    <RowList />
     <Footer/>  
     </>
    
--- netflix/src/Utils/axios.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const axios = () => {baseURL: "https://api.themoviedb.org/3",};
-
-export default axios;
